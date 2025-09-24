c0f7a888924592e185e9b9d950810f889cb0f57d
Athenr <82354163+Athenr@users.noreply.github.com>
2025-09-23 22:14:09 -0400

    Netlify was on the wrong version apparently, sorry.

--- Project1/ui-project-1/svelte.config.js
+++ Project1/ui-project-1/svelte.config.js
@@ -1,17 +1,14 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-netlify';
-
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			edge: false,
-			split: false
-		})
-	},
+  kit: {
+    adapter: adapter({
+      edge: false,
+      split: false
+    })
+  },
   preprocess: vitePreprocess()
 };
 
