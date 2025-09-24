8c73f2c20ef1b6355c5af27b47e4986aeaeb39e5
Athenr <82354163+Athenr@users.noreply.github.com>
2025-09-23 21:53:20 -0400

    Adding imports and stuff to host on Netlify

--- Project1/ui-project-1/svelte.config.js
+++ Project1/ui-project-1/svelte.config.js
@@ -1,8 +1,18 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			edge: false,
+			split: false
+		})
+	},
+  preprocess: vitePreprocess()
+};
+
+export default config;
